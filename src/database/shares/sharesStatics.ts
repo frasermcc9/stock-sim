e4d87017eb7ad13db70c06b55132e43a06e1694e
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-08 00:31:31 +1200

    fixed parameter names

--- src/database/shares/sharesStatics.ts
+++ src/database/shares/sharesStatics.ts
@@ -1,11 +1,11 @@
 import { IShareDocument, IShareModel } from "./sharesTypes";
 import { Client } from "../../Client";
 
-export async function findOneOrCreate(this: IShareModel, { id, shareSymbol }: { id: string; shareSymbol: string }): Promise<IShareDocument> {
-	const record = await this.findOne({ uId: id, symbol: shareSymbol });
+export async function findOneOrCreate(this: IShareModel, { uId, symbol }: { uId: string; symbol: string }): Promise<IShareDocument> {
+	const record = await this.findOne({ uId: uId, symbol: symbol });
 	if (record) {
 		return record;
 	} else {
-		return this.create({ uId: id, shares: 0, symbol: shareSymbol });
+		return this.create({ uId: uId, shares: 0, symbol: symbol });
 	}
 }
