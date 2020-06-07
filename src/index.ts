1b0e56882a75b88fca23a095e31871f12871dec4
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-08 00:30:21 +1200

    added client creation in index (temporary)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 import { connect } from "./database/database";
+import { Client } from "./Client";
+
+import * as auth from "../auth.json";
+
+Client.CreateClient({
+	dbName: "stocksimTest",
+	iexKey: auth.iexKey,
+	uri: auth.uri,
+	marketHoursOnly: false,
+	newUserValue: 1000,
+});
+
 connect();
