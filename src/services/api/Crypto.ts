dd059a64a03c630d051bfdc55afa7b4bb6f1bfa8
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-08 01:55:23 +1200

    class for crypto

---
+++ src/services/api/Crypto.ts
@@ -0,0 +1,19 @@
+import { BaseService } from "./BaseService";
+import { Quote, Company, Logo, CryptoQuote } from "node-iex-cloud/lib/types";
+
+export class Crypto extends BaseService {
+	private _symbol: string;
+
+	constructor(symbol: string) {
+		super();
+		this._symbol = symbol;
+	}
+
+	public async CurrentPrice(): Promise<number> {
+		return Number((await this.iex.crypto(this._symbol+"usd").quote()).latestPrice);
+	}
+	public async FullQuote(): Promise<CryptoQuote> {
+		return this.iex.crypto(this._symbol).quote();
+	}
+
+}
