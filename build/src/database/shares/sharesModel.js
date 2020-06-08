a78bdf14983f0f2b4b392c594b8ba7e3d9f25988
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-09 04:22:22 +1200

    latest build

--- build/src/database/shares/sharesModel.js
+++ build/src/database/shares/sharesModel.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const sharesSchema_1 = __importDefault(require("./sharesSchema"));
-exports.ShareModel = mongoose_1.model("user", sharesSchema_1.default);
+exports.ShareModel = mongoose_1.model("shares", sharesSchema_1.default);
 //# sourceMappingURL=sharesModel.js.map
