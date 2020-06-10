e961d32033c6a023cd416a3c0d5a05a942db5e73
Fraser McCallum <fraser@themccallums.net.nz>
2020-06-11 02:56:55 +1200

    changed folder name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Crypto } from "./services/api/Crypto";
 export { Symbol } from "./services/api/Symbol";
-export { UserAction } from "./services/local/UserAction";
+export { UserAction } from "./services/controllers/UserAction";
 export { connect } from "./database/database";
 export { Client } from "./Client";
 
